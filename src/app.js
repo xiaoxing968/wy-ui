cc9e8a9001d75b1c864890666e60cc00b321e3b2
xiaoxing968 <968xiaoxing@gmail.com>
2020-12-20 14:14:46 +0800

    安装了parcel

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import  Vue from 'vue'
+import Button from './button'
+Vue.component('w-button', Button)
+new Vue({
+    el:"#app"
+})
--- src/button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('w-button',{
-    template:`
-    <button class="w-button">我是一个按钮</button>
-    `
-})
