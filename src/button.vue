a107442870605b8e9c30f8b4e431c9d34bfe0367
xiaoxing968 <968xiaoxing@gmail.com>
2020-12-22 10:05:28 +0800

    icon图标 及 icon的position定位

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,46 @@
 <template>
-  <button class="w-button">我是一个按钮</button>
+  <button class="w-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
 export default {
-
+  // props:['icon','iconPosition'],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left"
+    }
+  }
 }
 </script>
 <style lang="scss">
-.w-button{
+.w-button {
   font-size: var(--font-size);
   height: var(--button-height);
-  padding:0 1em;
+  padding: 0 .7em;
   border-radius: var(--border-radius);
-  border:1px solid var(--border-color);
+  border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover{
-    border-color: var(--border-color-hover);
-  }
-  &:active{
-    background-color: var(--button-active-bg);
-  }
-  &:focus{
-    outline:none;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align:middle;
+  & .icon {width: 1em;height: 1em;}
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background-color: var(--button-active-bg);}
+  &:focus {outline: none;}
+    > .content {order: 2;}
+    > .icon {order: 1; margin-right: .2em}
+  &.icon-right {
+    > .content {order: 1;}
+    > .icon {order: 2;margin-left: .2em;margin-right: 0}
   }
 }
 </style>
