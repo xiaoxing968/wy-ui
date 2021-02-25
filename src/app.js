670080fc531914126dbb2c859ad6b18cc088d1ae
xiaoxing968 <968xiaoxing@gmail.com>
2021-02-25 10:27:02 +0800

    使用chai.expect添加4个测试用例

--- src/app.js
+++ src/app.js
@@ -2,12 +2,83 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import IconGroup from './button-group'
+
 Vue.component("w-button", Button)
 Vue.component("w-icon", Icon)
 Vue.component("w-button-group", IconGroup)
+
 new Vue({
     el:"#app",
     data:{
         loading: false,
+        loading1:false,
+        loading2:false,
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+
+// 单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement)
+    expect(useElement.getAttribute('xlink:href')).to.eq('#icon-setting')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    button.$mount('#test2')
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement)
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const  div = document.createElement("div")
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:"setting",
+            iconPosition:"right"
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const wButton = new Constructor({
+        propsData:{
+            icon:"setting",
+        }
+    })
+    wButton.$mount()
+    wButton.$on('click',function (){
+        console.log(1)
+    })
+    let button = wButton.$el
+    button.click()
+}
