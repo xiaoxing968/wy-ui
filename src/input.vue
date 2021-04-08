3061b4ae731af1f1affe98283c57a8a6a7f862d0
xiaoxing968 <968xiaoxing@gmail.com>
2021-04-08 20:24:07 +0800

    修改input样式和错误信息

--- src/input.vue
+++ src/input.vue
@@ -1,18 +1,41 @@
 <template>
- <div class="wrapper">
-   <input type="text">
- </div>
+  <div class="wrapper" :class="{error}">
+    <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <w-icon name="error" class="icon-error"></w-icon>
+      <span class="errorMessage">{{ error }}</span>
+    </template>
+  </div>
 </template>
 
-<script>
+<script>import wIcon from "./icon";
+
 export default {
-name: "wInput",
- data(){
- return{}
- },
- created(){},
- methods:{},
- components:{}
+  components: {wIcon},
+  name: "wInput",
+  props: {
+    value: {
+      type: String,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String,
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {},
 }
 
 </script>
@@ -20,30 +43,35 @@ name: "wInput",
 $height: 32px;
 $border-color: #999;
 $border-color-hover: #666;
-$border-radius :4px;
-$font-size: 12px;
-$box-shadow-color: rgba(0,0,0,0.5);
+$border-radius: 4px;
+$font-size: 14px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
 $red: #F1453D;
 .wrapper {
   font-size: $font-size;
   display: inline-flex;
   align-items: center;
+
   > :not(:last-child) {
     margin-right: .5em;
   }
+
   > input {
     height: 32px;
     border: 1px solid $border-color;
     border-radius: 4px;
     padding: 0 8px;
     font-size: inherit;
+
     &:hover {
       border-color: $border-color-hover;
     }
+
     &:focus {
       box-shadow: inset 0 1px 3px $box-shadow-color;
       outline: none;
     }
+
     &[disabled],
     &[readonly] {
       border-color: #bbb;
@@ -51,11 +79,20 @@ $red: #F1453D;
       cursor: not-allowed;
     }
   }
+
   &.error {
-    > input { border-color: $red; }
+    > input {
+      border-color: $red;
+    }
   }
-  .icon-error { fill: $red; }
-  .errorMessage { color: $red; }
+
+  .icon-error {
+    fill: $red;
+  }
+
+  .errorMessage {
+    color: $red;
+  }
 }
 
 </style>
