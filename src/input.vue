9fdda0e4fa31109be943f18cdd632ed89d6334f5
xiaoxing968 <968xiaoxing@gmail.com>
2021-04-08 17:59:02 +0800

    修改svg的问题

---
+++ src/input.vue
@@ -0,0 +1,18 @@
+<template>
+ <div></div>
+</template>
+
+<script>
+export default {
+name: "input",
+ data(){
+ return{}
+ },
+ created(){},
+ methods:{},
+ components:{}
+}
+
+</script>
+<style scoped>
+</style>
