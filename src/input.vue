756462581d5cdca01e7c50f9d9c683e18ac48901
xiaoxing968 <968xiaoxing@gmail.com>
2021-04-09 10:29:10 +0800

    修改input样式更换错误信息的icon图标

--- src/input.vue
+++ src/input.vue
@@ -8,8 +8,8 @@
   </div>
 </template>
 
-<script>import wIcon from "./icon";
-
+<script>
+import wIcon from "./icon";
 export default {
   components: {wIcon},
   name: "wInput",
@@ -47,52 +47,19 @@ $border-radius: 4px;
 $font-size: 14px;
 $box-shadow-color: rgba(0, 0, 0, 0.5);
 $red: #F1453D;
-.wrapper {
-  font-size: $font-size;
-  display: inline-flex;
-  align-items: center;
-
-  > :not(:last-child) {
-    margin-right: .5em;
-  }
-
-  > input {
-    height: 32px;
-    border: 1px solid $border-color;
-    border-radius: 4px;
-    padding: 0 8px;
-    font-size: inherit;
-
-    &:hover {
-      border-color: $border-color-hover;
-    }
-
-    &:focus {
-      box-shadow: inset 0 1px 3px $box-shadow-color;
-      outline: none;
-    }
-
+.wrapper {font-size: $font-size;display: inline-block;
+  //font-size: $font-size;display: inline-flex;align-items: center;
+  > :not(:last-child) {margin-right: .2em;}
+  > input {height: 32px;border: 1px solid $border-color;border-radius: 4px;padding: 0 8px;font-size: inherit;
+    &:hover {border-color: $border-color-hover;}
+    &:focus {box-shadow: inset 0 1px 3px $box-shadow-color;outline: none;}
     &[disabled],
-    &[readonly] {
-      border-color: #bbb;
-      color: #bbb;
-      cursor: not-allowed;
-    }
+    &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed;}
   }
-
   &.error {
-    > input {
-      border-color: $red;
-    }
+    > input {border-color: $red;}
   }
-
-  .icon-error {
-    fill: $red;
-  }
-
-  .errorMessage {
-    color: $red;
-  }
+  .icon-error {fill: $red;}
+  .errorMessage {color: $red;}
 }
-
 </style>
