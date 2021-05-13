2b0a35394673e5795b15a2795a93faecd38269d7
xiaoxing968 <968xiaoxing@gmail.com>
2021-05-13 19:48:47 +0800

    重构col 和 row

--- src/col.vue
+++ src/col.vue
@@ -1,44 +1,74 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]">
+  <div class="col" :class="colClasses" :style="colStyle">
+    <div style="border: 1px solid red">
       <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "w-col",
-  data() {
-    return {}
-  },
   props: {
     span: {
-      type: [Number, String]
+      type: [String, Number],
+      default: ''
     },
     offset: {
-      type: [Number, String]
+      type: [String, Number],
+      default: ''
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+  computed: {
+    colClasses() {
+      let {span, offset} = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
     },
-  },
-  created() {
-  },
-  methods: {},
-  components: {}
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px',
+      }
+    }
+  }
 }
-
 </script>
+
 <style scoped lang="less">
 .col {
-  height: 100px;
-  background: #bbbbbb;
+  height: 40px;
+  //background: @gray-2;
+  //border: 1px solid @gray-4;
+  border-radius: 4px;
   width: 50%;
-  border: 1px solid red;
-  .col-loop(@index) when (@index > 0) { /* recursive mixin with guard expression - condition */
-    /* the statement */
-    .col-@{index} {
-      width: (@index / 24) * 100%;
-    }
-    /* end of the statement */
-    .col-loop(@index - 1); /* the next iteration's call - final-expression*/
-  }
-  .col-loop(24);
+  box-sizing: border-box;
 }
+
+.col-loop(@index) when (@index > 0) { /* recursive mixin with guard expression - condition */
+  /* the statement */
+  .col-@{index} {
+    width: (@index / 24) * 100%;
+  }
+  /* end of the statement */
+  .col-loop(@index - 1); /* the next iteration's call - final-expression*/
+}
+
+.col-loop(24);
+.offset-loop(@index) when (@index > 0) { /* recursive mixin with guard expression - condition */
+  /* the statement */
+  .offset-@{index} {
+    margin-left: (@index / 24) * 100%;
+  }
+  /* end of the statement */
+  .offset-loop(@index - 1); /* the next iteration's call - final-expression*/
+}
+
+.offset-loop(24);
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,23 +1,36 @@
 <template>
- <div class="row">
-   <slot></slot>
- </div>
+  <div class="rol" :style="rowStyle">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-name: "row",
- data(){
- return{}
- },
- created(){},
- methods:{},
- components:{}
+  props: {
+    gutter: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  computed: {
+    rowStyle() {
+      let {gutter} = this
+      return {
+        marginLeft: -gutter / 2 + 'px',
+        marginRight: -gutter / 2 + 'px',
+      }
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    })
+  }
 }
-
 </script>
-<style scoped>
-.row{
+
+<style scoped lang="less">
+.rol {
   display: flex;
 }
 </style>
