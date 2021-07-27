83a8be5d39f67cbe8dbaec2392e6930dfb70a187
xiaoxing968 <968xiaoxing@gmail.com>
2021-07-27 10:24:28 +0800

    1.使用表驱动改写popover显示方式2.支持click、hover打开3.支持在popover内容里面关闭

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-popover-wrapper" @click="togglePopover" ref="popover">
+  <div class="w-popover-wrapper" ref="popover">
     <div class="w-popover-content" :class="contentClasses" ref="content" v-if="visible" @click.stop>
-      <slot name="content"></slot>
+      <slot name="content" :contentClose="contentClose"></slot>
     </div>
     <div class="w-popover-trigger" ref="trigger">
       <slot></slot>
@@ -21,6 +21,13 @@ export default {
       validator (value) {
         return [ 'top', 'left', 'bottom', 'right' ].indexOf(value) > -1
       }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator (value) {
+        return [ 'click', 'hover' ].indexOf(value) > -1
+      }
     }
   },
   computed: {
@@ -33,52 +40,98 @@ export default {
   },
   data () {
     return {
-      visible: false
+      visible: false,
+      closeTimeOut: null
+    }
+  },
+  mounted () {
+    let { popover } = this.$refs
+    switch (this.trigger) {
+      case 'click':
+        popover.addEventListener('click', this.onClick)
+        break
+      case 'hover':
+        popover.addEventListener('mouseenter', this.contentOpen)
+        popover.addEventListener('mouseleave', (event) => {
+          this.closeTimeOut = setTimeout(() => {
+            this.contentClose(event)
+          }, 100)
+        })
+        break
+    }
+  },
+  destroyed () {
+    let { popover, content } = this.$refs
+    switch (this.trigger) {
+      case 'click':
+        popover.removeEventListener('click', this.onClick)
+        break
+      case 'hover':
+        popover.removeEventListener('mouseenter', this.contentOpen)
+        popover.removeEventListener('mouseleave', this.contentClose)
+        content.removeEventListener('mouseenter', this.clearTimeout)
+        content.removeEventListener('mouseleave', this.contentClose)
+        break
     }
   },
   methods: {
     positionContent () {
       document.body.appendChild(this.$refs.content)
-      let { content, trigger } = this.$refs
-      let { top, left, height, width } = trigger.getBoundingClientRect()
-      let contentTop = top
-      let contentLeft = left
-      let { placement } = this
-      let { height:contentHeight } = content.getBoundingClientRect()
-      switch (placement) {
-        case 'bottom': contentTop = top + height; break
-        case 'right': contentLeft = left + width; break
+      const { content, trigger } = this.$refs
+      const { top, left, height, width } = trigger.getBoundingClientRect()
+      const { placement } = this
+      const { height:contentHeight } = content.getBoundingClientRect()
+      const leftRightTop = top - ( contentHeight - height ) / 2
+      let positions = {
+        top: { top, left },
+        bottom: { top: top + height, left },
+        left: { top: leftRightTop, left },
+        right: { top: leftRightTop, left: left + width }
       }
-      if (placement === 'left' || placement === 'right') {
-        contentTop = top - ( contentHeight - height ) / 2
-      }
-      content.style.top = contentTop + window.scrollY + 'px'
-      this.$refs.content.style.left = contentLeft + window.scrollX + 'px'
+      content.style.top = positions[placement].top + window.scrollY + 'px'
+      content.style.left = positions[placement].left + window.scrollX + 'px'
     },
     toggleHandler (e) {
       if (this.$refs.popover !== e.target && !this.$refs.popover.contains(e.target)) {
         this.contentClose()
       }
     },
-    contentShow () {
+    contentOpen () {
+      let { trigger } = this
       this.visible = true
+      if (trigger === 'hover') {
+        this.onHover()
+      }
       setTimeout(() => {
         this.positionContent()
-        document.addEventListener('click', this.toggleHandler)
+        if (trigger === 'click') {
+          document.addEventListener('click', this.toggleHandler)
+        }
       })
     },
     contentClose () {
       this.visible = false
-      document.removeEventListener('click', this.toggleHandler)
+      if (this.trigger === 'click') {
+        document.removeEventListener('click', this.toggleHandler)
+      }
     },
-    togglePopover (event) {
+    onHover () {
+      this.$nextTick(() => {
+        this.$refs.content.addEventListener('mouseenter', this.clearTimeout)
+        this.$refs.content.addEventListener('mouseout', this.contentClose)
+      })
+    },
+    onClick (event) {
       if (this.$refs.trigger.contains(event.target)) {
         if (this.visible) {
           this.contentClose()
         } else {
-          this.contentShow()
+          this.contentOpen()
         }
       }
+    },
+    clearTimeout () {
+      clearTimeout(this.closeTimeOut)
     }
   }
 }
@@ -114,25 +167,25 @@ export default {
   &.popover-content__top {
     margin-top: -10px;
     transform: translateY(-100%);
-    &::after, &&::before { left: 10px; }
+    &::after, &&::before { left: 10px; border-bottom: none; }
     &::before { top: 100%; border-top-color: @border-color-main; }
     &::after { top: calc(100% - 1px); border-top-color: @white; }
   }
   &.popover-content__left {
     transform: translateX(-105%);
-    &::after, &&::before { top: 50%; transform: translateY(-50%);}
+    &::after, &&::before { top: 50%; transform: translateY(-50%); border-right: none; }
     &::before { left: 100%; border-left-color: @border-color-main; }
     &::after { left: calc(100% - 1px); border-left-color: @white; }
   }
   &.popover-content__right {
     margin-left: @padding-sm + 2;
-    &::after, &&::before { top: 50%; transform: translateY(-50%);}
+    &::after, &&::before { top: 50%; transform: translateY(-50%); border-left: none;}
     &::before { right: 100%; border-right-color: @border-color-main; }
     &::after { right: calc(100% - 1px); border-right-color: @white; }
   }
   &.popover-content__bottom {
     margin-top: 10px;
-    &::after, &&::before { left: 10px; }
+    &::after, &&::before { left: 10px; border-top: none}
     &::before { bottom: 100%; border-bottom-color: @border-color-main; }
     &::after { bottom: calc(100% - 1px); border-bottom-color: @white; }
   }
