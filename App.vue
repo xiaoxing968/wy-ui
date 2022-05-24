6b4d5abde9ef966891e05c6291c5eeaf8c8c8e10
xiaoxing968 <968xiaoxing@gmail.com>
2022-05-24 23:47:17 +0800

    改为vue-cli

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div></div>
+</template>
+
+<script>
+export default {
+name: "demo",
+ data(){
+ return{}
+ },
+ created(){},
+ methods:{},
+ components:{}
+}
+
+</script>
+<style scoped>
+</style>
